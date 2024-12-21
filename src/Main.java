74c5a8cfd564f54475bc43db1e6480d96199c03b
huawei <romanec.aleksey@gmail.com>
2024-12-21 20:36:23 +0400

    HW-9.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Home Work-9");
+        System.out.println("Задание №1");
+        int[] amountOfMoney = {20000, 30000, 15000, 25000, 60000};
+        int sum = 0;
+        for (int element : amountOfMoney) {
+            sum += element;
+        }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
+
     }
 }
